e01f051fe108d32cf897ca666310a793633a9c04
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2017-08-04 15:05:17 -0500

    creating a messaging app

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 var port = 5000;
 
+var messages = [];
+
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+app.post('/newMessage', function (req,res){
+	console.log('Message post was hit!!');
+	messages.push(req.body);
+	console.log(messages);
+	res.sendStatus(201);
+});
+
+app.get('/updateMessage', function (req, res){
+	res.send(messages);
+});
 
 app.listen(port, function() {
 	console.log('listening on port', port);
--- public/client.js
+++ public/client.js
@@ -1,6 +1,40 @@
 console.log('client.js has been loaded');
 
-$(document).ready(function(){
+$(document).ready(function () {
     console.log('jQuery has been loaded');
- 
+
+    $('#sendMessageButton').on('click', function () {
+        console.log('Message button was clicked!');
+        var nameInput = $('#nameInput').val();
+        var messageInput = $('#messageInput').val();
+
+        $.ajax({
+            method: 'POST',
+            url: '/newMessage',
+            data: {
+                name: nameInput,
+                message: messageInput
+            },
+            success: function (response) {
+                $('#messageInput').val('');
+                console.log(response);
+            }
+        })
+        //postMessage();
+    });
+
 });
+
+//$('#messageInput').val('');
+
+
+// function postMessage() {
+//     $.ajax({
+//         method: 'GET',
+//         url: '/updateMessage',
+//         success: function (response) {
+//             console.log(response);
+//             $('#messageDiv').prepend(response);
+//         }
+//     })
+// }
